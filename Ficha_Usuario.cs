fa976555b882c451eb145fbb0b46382e3f8ffb2a
Alexander2119m <105405177+Alexander2119m@users.noreply.github.com>
2022-06-13 22:40:19 -0500

    Add files via upload

---
+++ Ficha_Usuario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETAPA2B
+{
+     class Ficha_Usuario:ClassPadre
+    {
+        public string Mpago { set; get; }
+
+        public Ficha_Usuario (int ID, int Cedula, string Nombre, string Apellido, string Correo, int Edad, int Telefono, string Direccion, string Mpago) : base(ID, Cedula, Nombre, Apellido, Correo, Edad, Telefono, Direccion)
+        {
+            this.Mpago = Mpago;
+        }
+
+        public override void ImprimirDatos()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Identificacion: " + ID);
+            Console.WriteLine("Numero de Cedula " + Cedula);
+            Console.WriteLine(" Nombre : " + Nombre);
+            Console.WriteLine("Apellidos :" + Apellido);
+            Console.WriteLine("Edad  :" + Edad);
+            Console.WriteLine("Correo Electronico :" + Correo);
+            Console.WriteLine("Numero de telefono :" + Telefono);
+            Console.WriteLine("Direccion del domicilio :" + Direccion);
+        }
+
+        public override void pago()
+        {
+            
+            double operar, credito,final , resultado , debito , resul;
+            operar = 1700 * 12 /100 ;
+            final = 1700 + operar;
+            credito = 1700 * 40 / 100;
+            resultado = 1700 + credito;
+            debito = 1700 * 18 / 100;
+            resul = 1700 + debito;
+            Console.WriteLine("Forma de pago :" + Mpago);
+            Console.WriteLine("El precio inicial de la portatil :" + 1700);
+            Console.WriteLine("Estas son las diferentes fomras de pago con sus debidos interes");
+            Console.WriteLine("Precio en efectivo :" + final  );
+            Console.WriteLine("Precio en tarjeta de credito  :" + resultado);
+            Console.WriteLine("Precio en tarjeta de debito  :" + resul);
+
+            
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ETAPA2B
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ficha_Usuario usuario = new Ficha_Usuario (5458636, 1305468593, " Pedro ", " Cevallos ", " pedro1998@gmail.com ", 25, 0998231547, " Calle 312 Av 217 ", " Detallar las formas de pago " );
+            usuario.ImprimirDatos();
+            usuario.pago();
+            
+
+            Ficha_Vendedor vendedor = new Ficha_Vendedor( 7896356 ,  1307895622 , " Juan ", " Gomez ", " juan1975@gmail.com ",  35  ,  0989695223 , " Calle 320 Av 117 ", " Mayor edad , Experencia Previa ,  Aprendizaje proactivo    ", " Ventas de laptops " , " Catalogo de laptops ");
+            vendedor.ImprimirDatos();
+            vendedor.Servicio();
+            vendedor.venta();
+            vendedor.legales();
+
+            Ficha_Administrador Administrador = new Ficha_Administrador( 457896 ,  130235784 , " Jose ", " Arteaga ", " jose23@gmail.com ",  48   , 0989695223 , " Ciudadela Universitaria ", " Contar con una experiencia laboral de 5 años  ", " Ingeniero Tecnologia de la Informacion  ");
+            Administrador.ImprimirDatos();
+            Administrador.Servicio();
+
+            Requisitos Req = new Requisitos();
+            Req.Requisito();
+        }
+    }
+}
